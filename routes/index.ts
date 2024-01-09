8ee98e37b14cfc0f7d165674cb7965477fb02b8e
belkud <vlad-belkud@yandex.ru>
2024-01-09 13:32:03 +0300

    gk

--- routes/index.ts
+++ routes/index.ts
@@ -4,7 +4,8 @@ const router = Router()
 // *** Test ***
 router.get('/', (request, response) => {
   response.send({
-    message: 'Node.js and Express REST API'
+    message: 'Node.js and Express REST API',
+    message2: 'Привет Вова'
   })
 })
 
